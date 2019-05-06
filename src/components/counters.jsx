bacb87919426142dcddc7b3fd43eb64d7c447562
Danylo Zasymenko <dzasymenko@ancestry.com>
2019-05-06 10:02:29 +0300

    added files

---
+++ src/components/counters.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import Counter from "./counter";
+
+export default class Counters extends Component {
+    render() {
+        console.log("Counters - Rendered");
+
+        const {onReset, counters, onDelete, onIncrement} = this.props;
+
+        return (
+            <div>
+                <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
+                    Reset
+                </button>
+                {counters.map(counter =>
+                    <Counter
+                        key={counter.id}
+                        counter={counter}
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}
+                    >
+                        <h6>Counter #{counter.id}</h6>
+                    </Counter>
+                )}
+            </div>
+        );
+    }
+}
